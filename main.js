227b1c2541ea80e3745c7237ffd0a5b7387574f5
Aaron <trollolol1029@gmail.com>
2018-12-08 16:50:23 +0800

    fix

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
   $("#portfolio").load("w3-col m3", function() {
     setTimeout(function(){
      $('body').addClass('loaded');}
-     ,100);
+     ,3000);
    });
 });
