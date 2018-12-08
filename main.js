7ba9c53e462423e764a5a948922c028579d45614
Aaron <trollolol1029@gmail.com>
2018-12-08 16:54:12 +0800

    test

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $("#portfolio").load("w3-col m3", function() {
+  $("#portfolio").load("w3-row-padding w3-center w3-section", function() {
     setTimeout(function(){
      $('body').addClass('loaded');}
      ,3000);
