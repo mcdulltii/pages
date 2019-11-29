6fe1c73a56eb4bf89f8c039382a125395d910f5d
mcdulltii <mcdulltii@gmail.com>
2019-11-29 20:43:05 +0800

    test
    Former-commit-id: cae66c9322cd1fe41a9437a84523c0677caa2702

--- scrollify.js
+++ scrollify.js
@@ -9,4 +9,17 @@ $(function() {
     e.preventDefault();
     $.scrollify.next();
   });
+
+  $(".top").click(function(e) {
+    e.preventDefault();
+    $.scrollify.move("#1");
+  });
 });
+
+window.onscroll = function() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementsByClassName("top").style.display = "block";
+  } else {
+    document.getElementsByClassName("top").style.display = "none";
+  }
+};
