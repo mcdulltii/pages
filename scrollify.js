2c13019a0964adeba8c6af0ffd44447cbb23a7d8
mcdulltii <mcdulltii@gmail.com>
2019-11-29 23:15:45 +0800

    test
    Former-commit-id: 213cddf8f8a1b624bf3f763dcab964c65c64e462

--- scrollify.js
+++ scrollify.js
@@ -1,28 +1,30 @@
-var videolist = document.getElementById("videos");
-var navbar = document.getElementById("nav");
-var vidlength = videolist.children.length+1;
-for (var i=1; i<vidlength; i++) {
-  var dot = document.createElement("li");
-  dot.className = "dot "+i.toString();
-  navbar.appendChild(dot);
-}
-
-$(function() {
-  $.scrollify({
-    section : ".vidsect",
-    scrollSpeed: 500,
-    scrollbars: false
-  });
-
-  $(".scroll,.scroll-btn").click(function(e) {
-    e.preventDefault();
-    $.scrollify.next();
-  });
-
+if(window.innerWidth > 1024) {
+  var videolist = document.getElementById("videos");
+  var navbar = document.getElementById("nav");
+  var vidlength = videolist.children.length+1;
   for (var i=1; i<vidlength; i++) {
-    $("."+i.toString()).click(function(e) {
-      e.preventDefault();
-      $.scrollify.move("#"+($(e.target).attr('class')).match(/\d+/)[0]);
-    });
+    var dot = document.createElement("li");
+    dot.className = "dot "+i.toString();
+    navbar.appendChild(dot);
   }
-});
+
+  $(function() {
+    $.scrollify({
+      section : ".vidsect",
+      scrollSpeed: 500,
+      scrollbars: false
+    });
+
+    $(".scroll,.scroll-btn").click(function(e) {
+      e.preventDefault();
+      $.scrollify.next();
+    });
+
+    for (var i=1; i<vidlength; i++) {
+      $("."+i.toString()).click(function(e) {
+        e.preventDefault();
+        $.scrollify.move("#"+($(e.target).attr('class')).match(/\d+/)[0]);
+      });
+    }
+  });
+}
