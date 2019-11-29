7e9935feecb246603b10270787550c0bd52c5dc7
mcdulltii <mcdulltii@gmail.com>
2019-11-29 20:46:42 +0800

    test
    Former-commit-id: 063ee18e631d25413e7dd73b48fc37f66d28c9de

--- scrollify.js
+++ scrollify.js
@@ -10,7 +10,7 @@ $(function() {
     $.scrollify.next();
   });
 
-  $(".top").click(function(e) {
+  $("#myBtn").click(function(e) {
     e.preventDefault();
     $.scrollify.move("#1");
   });
@@ -18,8 +18,8 @@ $(function() {
 
 window.onscroll = function() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementsByClassName("top").style.display = "block";
+    document.getElementById("myBtn").style.display = "block";
   } else {
-    document.getElementsByClassName("top").style.display = "none";
+    document.getElementById("myBtn").style.display = "none";
   }
 };
