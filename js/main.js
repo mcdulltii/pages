3e8199eb99f5b057b346f69fb2397200b72e9293
mcdulltii <mcdulltii@gmail.com>
2019-12-01 16:32:56 +0800

    totop
    Former-commit-id: 9d84e324e3353e4fbfecdc3972f278bce6740eab

--- js/main.js
+++ js/main.js
@@ -6,8 +6,8 @@ function toTop() {
 $(document).ready(function() {
   $("#portfolio").load("w3-row-padding w3-center w3-section", function() {
     setTimeout(function(){
-     $('body').addClass('loaded');}
-     ,3000);
+     $('body').addClass('loaded');
      toTop();
+    },3000);
    });
 });
